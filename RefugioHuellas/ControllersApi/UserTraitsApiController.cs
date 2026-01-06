a7479e0b04d4a09ed85f6e371a9ebac71ba7824a
NotGuatas <josephaugustoflores@gmail.com>
2026-01-06 11:26:04 -0500

    Arreglo 2

--- RefugioHuellas/ControllersApi/UserTraitsApiController.cs
+++ RefugioHuellas/ControllersApi/UserTraitsApiController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +12,7 @@ namespace RefugioHuellas.ControllersApi
 {
     [ApiController]
     [Route("api/user-traits")]
-    [Authorize] // requiere JWT
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class UserTraitsApiController : ControllerBase
     {
         private readonly ApplicationDbContext _db;
